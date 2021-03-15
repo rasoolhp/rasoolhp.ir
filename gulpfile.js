167a82c89c0c61ceee907d5c8063641121ce3fb7
rasoolhp <rasoolhp4@gmail.com>
2021-03-15 14:38:01 +0330

    🍹 Now we have Gulp

---
+++ gulpfile.js
@@ -0,0 +1,52 @@
+const gulp = require('gulp');
+const fileinclude = require('gulp-file-include');
+const {
+  watch,
+  series
+} = require('gulp');
+
+const paths = {
+  scripts: {
+    src: './',
+    dest: './docs/'
+  }
+};
+
+// Build HTML files
+async function includeHTML() {
+  return gulp.src([
+      '*.html',
+      '!head.html', // ignore
+    ])
+    .pipe(fileinclude({
+      prefix: '@@',
+      basepath: '@file'
+    }))
+    .pipe(gulp.dest(paths.scripts.dest));
+}
+
+// Copy assets
+async function copyAssets() {
+  gulp.src(['dist/**/*'], {
+      base: './'
+    })
+    .pipe(gulp.dest(paths.scripts.dest));
+}
+
+// Build HTML files and Copy assets
+async function buildAndReload() {
+  // NOTE: If you want, You can add await before these functions.
+  includeHTML();
+  copyAssets();
+  // TODO: add live refresh server
+}
+
+
+exports.includeHTML = includeHTML;
+
+exports.default = async function() {
+  // Build and reload at the first time
+  buildAndReload();
+  // Watch task
+  watch(["*.html", "dist/**/*"], series(buildAndReload));
+};
