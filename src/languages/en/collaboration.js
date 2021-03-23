e8374ba133546946711642fedb37197fc2035a56
rasoolhp <rasoolhp4@gmail.com>
2021-03-23 21:18:20 +0430

    🍹 Add collaboration page and translations and FINISH!

---
+++ src/languages/en/collaboration.js
@@ -0,0 +1,11 @@
+define({
+    canonical:"https://rasoolhp.ir/en/collaboration/",
+    ways:"Models of Collaboration with me",
+    hourly:"• Hourly",
+    project:"• Freelancing",
+    consulting :"• Consulting",
+    monitoring :"• Supervising",
+    startup:"• Startup",
+    abilitiesDesc:"If you want to know about my abilities, Click on this button:",
+    vcardDesc:"Would you like to collaborate with me? So contact me:",
+});
---
+++ src/languages/fa/collaboration.js
@@ -0,0 +1,11 @@
+define({
+    canonical:"https://rasoolhp.ir/fa/collaboration/",
+    ways:"مدل های همکاری با من",
+    hourly:"• استخدام ساعتی",
+    project:"• استخدام پروژه‌ای",
+    consulting :"• مشاوره",
+    monitoring :"• نظارت",
+    startup:"• استارتاپی",
+    abilitiesDesc:"اگر می‌خواین در مورد توانایی های من بدونین، سری به این صفحه بزنید.",
+    vcardDesc:"اگر مایل به همکاری با من هستید با من تماس بگیرید.",
+});
